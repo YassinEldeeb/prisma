3b3103acfa08143b56dec5069b5f7618f374ae84
github-action-benchmark <github@users.noreply.github.com>
2022-04-18 15:40:50 +0000

    add Benchmark.js Benchmark (benchmarkjs) benchmark result for 8f6b7c7c99c1c720cf5bfed5d563423e71c1b84f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,6 +1,6 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1639034854744,
-  "repoUrl": "https://github.com/prisma/prisma",
+  "lastUpdate": 1650296450841,
+  "repoUrl": "https://github.com/YassinEldeeb/prisma",
   "entries": {
     "Benchmark.js Benchmark": [
       {
@@ -70411,6 +70411,72 @@ window.BENCHMARK_DATA = {
           {
             "name": ".prisma/client size",
             "value": 45,
+            "range": "±0.00%",
+            "unit": "MB",
+            "extra": "1 samples"
+          },
+          {
+            "name": ".prisma/client/index.d.ts size",
+            "value": 1.7,
+            "range": "±0.00%",
+            "unit": "MB",
+            "extra": "1 samples"
+          },
+          {
+            "name": ".prisma/client/index.js size",
+            "value": 3.1,
+            "range": "±0.00%",
+            "unit": "MB",
+            "extra": "1 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "piotrowski+github@gmail.com",
+            "name": "Jan Piotrowski",
+            "username": "janpio"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8f6b7c7c99c1c720cf5bfed5d563423e71c1b84f",
+          "message": "ci(cli): Increase timeout of \"using cli > should work with a custom generator\" test to avoid flakyness (mac) (#12828)",
+          "timestamp": "2022-04-15T19:33:51+02:00",
+          "tree_id": "151dbdfeb9b07ebd32b777626adfd36b77fe36f0",
+          "url": "https://github.com/YassinEldeeb/prisma/commit/8f6b7c7c99c1c720cf5bfed5d563423e71c1b84f"
+        },
+        "date": 1650296446592,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "client generation ~50 Models",
+            "value": 0.45,
+            "range": "±4.62%",
+            "unit": "ops/sec",
+            "extra": "7 samples"
+          },
+          {
+            "name": "typescript compilation ~50 Models",
+            "value": 0.11,
+            "range": "±3.33%",
+            "unit": "ops/sec",
+            "extra": "5 samples"
+          },
+          {
+            "name": "@prisma/client size",
+            "value": 8.4,
+            "range": "±0.00%",
+            "unit": "MB",
+            "extra": "1 samples"
+          },
+          {
+            "name": ".prisma/client size",
+            "value": 41,
             "range": "±0.00%",
             "unit": "MB",
             "extra": "1 samples"
